96ccf5ea87e9ec1390737cf597ef0952d276314b
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-08-10 16:29:39 -0600

    finished traversy guide

--- src/App.js
+++ src/App.js
@@ -7,6 +7,47 @@ import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField";
+
+import {makeStyles, ThemeProvider, createTheme} from "@material-ui/core/styles";
+import { orange, green } from "@material-ui/core/colors";
+import "fontsource-roboto";
+
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles ({
+  root: {
+    background: 'linear-gradient(45deg, #333, #999)',
+    border: 0,
+    borderRadius: 15,
+    marginBottom: 15, 
+    color: 'white',
+    padding: '10px 30px'
+  }
+})
+
+const theme = createTheme({
+  typography: {
+    h2: {
+      fontSize: 36,
+      marginBottom: 15,
+    }
+  },
+})
+
+function ButtonStyled () {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = React.useState(true);
@@ -28,23 +69,66 @@ function CheckboxExample() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <CheckboxExample />
-        <ButtonGroup variant="contained" color="primary" size="large">
-          <Button
-            startIcon={<SaveIcon />}
-          >
-            Save
-          </Button>
-          <Button
-            startIcon={<DeleteIcon />}
-          >
-            Discard
-          </Button>
-        </ButtonGroup>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="lg">
+        <div className="App">
+          <header className="App-header">
+            <AppBar color="secondary">
+              <Toolbar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">
+                  MUI Theming
+                </Typography>
+                <Button>
+                  Login
+                </Button>
+              </Toolbar>
+            </AppBar>
+            <Typography variant="h2" component="div">
+              Welcome to MUI
+            </Typography>
+            <Typography variant="subtitle1">
+              Learn how to use Material UI
+            </Typography>
+            <ButtonStyled />
+            <Grid container spacing={2} justify="center">
+              <Grid item xs={3} sm={6}>
+                <Paper style={{height: 75, width: '100%',}}/>
+              </Grid>
+              <Grid item xs={3} sm={6}>
+                <Paper style={{height: 75, width: '100%',}}/>
+              </Grid>
+              <Grid item xs={3} lg={12}>
+                <Paper style={{height: 75, width: '100%',}}/>
+              </Grid>
+            </Grid>
+
+            {/* <TextField 
+              variant="filled"
+              color="secondary"
+              type="email"
+              label="The Time"
+              placeholder="test@test.com"
+            /> */}
+            <CheckboxExample />
+            <ButtonGroup variant="contained" color="primary" size="large">
+              <Button
+                startIcon={<SaveIcon />}
+              >
+                Save
+              </Button>
+              <Button
+                startIcon={<DeleteIcon />}
+              >
+                Discard
+              </Button>
+            </ButtonGroup>
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
